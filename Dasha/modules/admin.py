eb45631fc574007c957cda483b6d5a5cc3595ba6
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-14 19:47:39 +0530

    update admin.py

--- Dasha/modules/admin.py
+++ Dasha/modules/admin.py
@@ -22,8 +22,10 @@ async def kick(event):
 
 @dasha(pattern="^/pin$")
 async def pin(event):
-    fuck = await event.edit('`Processing..`')
-    lol = await event.get_reply_message()
-    await event.client.pin_message(event.chat_id, lol, notify=True)
-    await fuck.edit(x,f'**Sᴜᴄᴄᴇssғᴜʟʟʏ Pɪɴɴᴇᴅ [Tʜɪs](http://t.me/c/{x.chat.id}/{x.id}) ᴍᴇssᴀɢᴇ**')
+     fuck = await event.edit('`Processing..`')
+     lol = await event.get_reply_message()
+     await event.client.pin_message(event.chat_id, lol, notify=True)
+     await fuck.edit(x,f'**Sᴜᴄᴄᴇssғᴜʟʟʏ Pɪɴɴᴇᴅ [Tʜɪs](http://t.me/c/{x.chat.id}/{x.id}) ᴍᴇssᴀɢᴇ**')
+  except:
+      await event.edit("Can't kick admins.")
   
