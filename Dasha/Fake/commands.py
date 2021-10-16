5da493399f8a3a6c18cf4b9ff20f61d121db7cbb
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-17 02:15:13 +0530

    Delete Dasha/Fake directory

--- Dasha/Fake/commands.py
+++
@@ -1,7 +0,0 @@
-#soon
-from telethon import events
-from Dasha import hbot
-
-@hbot.on(events.NewMessage(pattern="[/]start"))
-async def fuck(event):
-      await hbot.send_message(event.chat_id, '/hunt for pokemon') 
