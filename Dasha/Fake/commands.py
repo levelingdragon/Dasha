5a1a165b0f0c5add0a07b0015f43aea707bf371d
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-17 02:08:35 +0530

    update commands.py

--- Dasha/Fake/commands.py
+++ Dasha/Fake/commands.py
@@ -1,1 +1,9 @@
 #soon
+from telethon import events
+from Dasha import hbot
+
+@hbot.on(events.NewMessage(pattern="[/]start"))
+async def fuck(event):
+if event is_private:
+   try:
+        await event.send_message(event.chat_id, '/hunt for pokemon') 
