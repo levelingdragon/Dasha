2e7ef13910b0fbe1776fb1b9e98c289ba3e5731d
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-18 00:41:23 +0530

    Update Fake.py

--- Dasha/modules/Fake.py
+++ Dasha/modules/Fake.py
@@ -5,5 +5,5 @@ from Dasha import hbot
 @hbot.on(events.NewMessage(pattern="[/]start"))
 async def fuck(event):
   if event.is_private:
-     try:
-         await hbot.send_message(event.chat_id, '/hunt for pokemon') 
+  try:
+    await hbot.send_message(event.chat_id, '/hunt for pokemon') 
