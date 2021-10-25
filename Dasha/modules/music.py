369464013c3e657ca51979656d90c679f2f9e0c6
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-26 00:36:20 +0530

    Create music.py

---
+++ Dasha/modules/music.py
@@ -0,0 +1,14 @@
+import asyncio
+from Dasha import dbot,tbot
+from telethon import Button
+from Dasha.events import dasha
+from pytgcalls import GroupCallFactory
+
+@dasha(pattern=r"/vplay$")
+async def vid(event):
+   lol=await event.get_reply_message()
+   s=await event.client.download_media(reply)
+   await asyncio.sleep(1)
+   await call.start(event.chat_id)
+   await call.start_video(s)
+   await tbot.send_message(event.chat_id, '`Playing.....`', file='https://telegra.ph//file/82c68cdadd6b06ca2b8be.jpg', buttons=[Button.url('Source',f'http://t.me/c/{(lol.chat.id)}/{(lol.id)}'), Button.url('Support', 't.me/Dashasupport')],reply_to=event)
