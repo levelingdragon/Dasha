13b5d14fe9878593aa2fa16295bb818a57edfa28
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-26 13:18:28 +0530

    Update music.py

--- Dasha/modules/music.py
+++ Dasha/modules/music.py
@@ -1,16 +1,15 @@
 import asyncio
-from Dasha import dbot,tbot
+from Dasha import xbot,tbot
 from telethon import Button
 from Dasha.events import dasha
 from pytgcalls import GroupCallFactory
 
 @dasha(pattern="^/vplay$")
 async def vid(event):
-   call = GroupCallFactory(dbot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON).get_group_call()
+   call = GroupCallFactory(xbot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON).get_group_call()
    lol=await event.get_reply_message()
    x=await tbot.send_message(event.chat_id,'`Downloading....`',reply_to=event)
    s=await event.client.download_media(reply)
-   await asyncio.sleep(1)
    await call.start(event.chat_id)
    await call.start_video(s)
    await x.delete()
