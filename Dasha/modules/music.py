b39d1f42c9650256b153ce55ba3379b08cd5b7d7
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-26 13:27:04 +0530

    Update music.py

--- Dasha/modules/music.py
+++ Dasha/modules/music.py
@@ -15,9 +15,9 @@ async def vid(event):
    await x.delete()
    await tbot.send_message(event.chat_id, '`Playing.....`', file='https://telegra.ph//file/82c68cdadd6b06ca2b8be.jpg', buttons=[Button.url('Source',f'http://t.me/c/{(lol.chat.id)}/{(lol.id)}'), Button.url('Support', 't.me/Dashasupport')],reply_to=event)
 
-   
-   @dasha(pattern="^/aplay$")
-async def vid(event):
+  
+@dasha(pattern="^/aplay$")
+async def audio(event):
    call = GroupCallFactory(xbot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON).get_group_call()
    lol=await event.get_reply_message()
    x=await tbot.send_message(event.chat_id,'`Downloading....`',reply_to=event)
