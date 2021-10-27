30535cba268150b9ef6e4815378dd9716efc9446
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-27 13:17:53 +0530

    update misc.py

--- Dasha/modules/misc.py
+++ Dasha/modules/misc.py
@@ -1,5 +1,6 @@
 from Dasha.events import dasha
 import speedtest
+import nekos
 #Some misc modules
 
 @dasha(pattern='^/stat')
@@ -22,3 +23,76 @@ async def lmao(event):
       sex=await event.edit('`Downloading...`')
       await event.client.download_media(x,f'{event.id}.mp4')
       await sex.edit('`Media Successfully dowmloaded.`')
+arguments = [
+    "feet",
+    "yuri",
+    "trap",
+    "futanari",
+    "hololewd",
+    "lewdkemo",
+    "solog",
+    "feetg",
+    "cum",
+    "erokemo",
+    "les",
+    "wallpaper",
+    "lewdk",
+    "ngif",
+    "tickle",
+    "lewd",
+    "feed",
+    "gecg",
+    "eroyuri",
+    "eron",
+    "cum_jpg",
+    "bj",
+    "nsfw_neko_gif",
+    "solo",
+    "nsfw_avatar",
+    "gasm",
+    "poke",
+    "anal",
+    "slap",
+    "hentai",
+    "avatar",
+    "erofeet",
+    "holo",
+    "keta",
+    "blowjob",
+    "pussy",
+    "tits",
+    "holoero",
+    "lizard",
+    "pussy_jpg",
+    "pwankg",
+    "classic",
+    "kuni",
+    "waifu",
+    "pat",
+    "8ball",
+    "kiss",
+    "femdom",
+    "neko",
+    "spank",
+    "cuddle",
+    "erok",
+    "fox_girl",
+    "boobs",
+    "random_hentai_gif",
+    "smallboobs",
+    "hug",
+    "ero",
+    "goose",
+    "baka",
+    "woof",
+    "kemonomimi",
+    "smug",
+]
+@dasha(pattern="^/nekos ?(.*)")
+async def nekos_img(event):
+    args = event.pattern_match.group(1)
+    anos = await event.reply("Fetching from nekos...")
+    pic = nekos.img(args)
+    await event.client.send_file(event.chat_id,pic)
+    await anos.delete()     
+ 
