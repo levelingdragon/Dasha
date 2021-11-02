3512e7b7e598905d9b68f8239e0d997e6688f003
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-11-02 15:39:27 +0530

    update music.py

--- Dasha/modules/music.py
+++ Dasha/modules/music.py
@@ -1,25 +1,17 @@
 import asyncio
-from Dasha import xbot,tbot
+from Dasha import dbot,tbot
 from telethon import Button
 from Dasha.events import dasha
 from pytgcalls import GroupCallFactory
 
 @dasha(pattern="^/vplay$")
 async def vid(event):
-   call = GroupCallFactory(xbot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON).get_group_call()
+   call = GroupCallFactory(dbot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON).get_group_call()
    lol=await event.get_reply_message()
    x=await tbot.send_message(event.chat_id,'`Downloading....`',reply_to=event)
-   s=await xbot.download_media(lol)
+   s=await event.client.download_media(lol, 'ok.mp4')
+   await asyncio.sleep(1)
    await call.start(event.chat_id)
-   await call.start_video(s)
-  
-@dasha(pattern="^/aplay$")
-async def audio(event):
-   call = GroupCallFactory(xbot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON).get_group_call()
-   lol=await event.get_reply_message()
-   x=await tbot.send_message(event.chat_id,'`Downloading....`',reply_to=event)
-   ok=await xbot.download_media(lol)
-   await call.start(event.chat_id)
-   await call.start_audio(ok)
+   await call.start_video('ok.mp4')
    await x.delete()
    await tbot.send_message(event.chat_id, '`Playing.....`', file='https://telegra.ph//file/82c68cdadd6b06ca2b8be.jpg', buttons=[Button.url('Source',f'http://t.me/c/{(lol.chat.id)}/{(lol.id)}'), Button.url('Support', 't.me/Dashasupport')],reply_to=event)
