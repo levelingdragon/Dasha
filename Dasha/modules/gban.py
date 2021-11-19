28f77885a54702a5d7c4b5cd6f3d3c97dc1c84f5
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-11-20 02:28:40 +0530

    Create gban.py

---
+++ Dasha/modules/gban.py
@@ -0,0 +1,14 @@
+from Dasha.events import dasha
+from Dasha.Sylviorus import update_gban as k
+@dasha(pattern="[*?]bl ?(.*)")
+async def _(e):
+ if e.is_reply:
+   i=await e.reply('`Gbanning`')
+   ok = await e.get_reply_message()
+   sad = (e.pattern_match.group(1))
+   send = (ok.sender.id)
+   lol = (e.sender.id)
+   await k(victim=sender, reason=sed, message= ok.text, enforcer=lol)
+   await i.edit('**Gbanned succesfully**')
+ else:
+   await e.respond('`Please reply to a message')
