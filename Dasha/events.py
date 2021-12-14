580a68c7d25aa644e85ed71a5e99402324a0c02c
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-12-15 02:24:41 +0530

    Update events.py

--- Dasha/events.py
+++ Dasha/events.py
@@ -5,7 +5,7 @@ from telethon import events
 
 def dasha(**args):
    pattern = args.get('pattern', None)
-   r_pattern = r'^[+*"]'
+   r_pattern = r'^[*"]'
    if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
    args['pattern'] = pattern.replace('^/', r_pattern, 1)
